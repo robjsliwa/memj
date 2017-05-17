c88c12d6e601717f467a2209af9b0bde672f61f3
Rob Sliwa <robjsliwa@gmail.com>
2017-05-17 14:55:02 -0400

    Memj initial code

---
+++ memj.go
@@ -0,0 +1,141 @@
+package memj
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+// MemJ - memory json
+type MemJ struct {
+	mutexLock       sync.RWMutex
+	collectionLocks map[string]*sync.RWMutex
+	data            map[string][]map[string]interface{}
+}
+
+// New - create new instance of MemJ
+func New() (*MemJ, error) {
+	memj := &MemJ{
+		collectionLocks: make(map[string]*sync.RWMutex),
+		data:            make(map[string][]map[string]interface{}),
+	}
+
+	return memj, nil
+}
+
+// Insert - insert json payload to collection
+func (m *MemJ) Insert(collection string, payload map[string]interface{}) (string, error) {
+	lock := m.getCollectionLock(collection)
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	objectID := uuid.New().String()
+	payload["objectid"] = objectID
+	m.data[collection] = append(m.data[collection], payload)
+
+	return objectID, nil
+}
+
+// Find - find collection with objectId in collection
+func (m *MemJ) Find(collection, objectID string) (map[string]interface{}, error) {
+	lock := m.getCollectionLock(collection)
+
+	lock.RLock()
+	defer lock.RUnlock()
+
+	for _, value := range m.data[collection] {
+		if value["objectid"] == objectID {
+			return value, nil
+		}
+	}
+
+	return nil, errors.New("Not found")
+}
+
+// FindAll - return all documents in the collection
+func (m *MemJ) FindAll(collection string) ([]map[string]interface{}, error) {
+	lock := m.getCollectionLock(collection)
+
+	lock.RLock()
+	defer lock.RUnlock()
+
+	return m.data[collection], nil
+}
+
+// Update - update existing object identified by objectID
+func (m *MemJ) Update(collection, objectID string, payload map[string]interface{}) (bool, error) {
+	lock := m.getCollectionLock(collection)
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	for index, value := range m.data[collection] {
+		if value["objectid"] == objectID {
+			payload["objectid"] = objectID
+			m.data[collection][index] = payload
+			return true, nil
+		}
+	}
+
+	return false, errors.New("Not found")
+}
+
+// Delete - delete object in collection identified by objectID
+func (m *MemJ) Delete(collection, objectID string) (bool, error) {
+	lock := m.getCollectionLock(collection)
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	for index, value := range m.data[collection] {
+		if value["objectid"] == objectID {
+			m.data[collection] = append(m.data[collection][:index], m.data[collection][index+1:]...)
+			return true, nil
+		}
+	}
+
+	return false, errors.New("Not found")
+}
+
+// Query - query for object in collection
+func (m *MemJ) Query(collection string, query map[string]interface{}) ([]map[string]interface{}, error) {
+	lock := m.getCollectionLock(collection)
+
+	lock.RLock()
+	defer lock.RUnlock()
+
+	var result []map[string]interface{}
+
+	for _, value := range m.data[collection] {
+		isFound := false
+		for k, v := range value {
+			if query[k] == v {
+				isFound = true
+			}
+		}
+
+		if isFound {
+			result = append(result, value)
+		}
+	}
+
+	return result, nil
+}
+
+func (m *MemJ) getCollectionLock(collection string) *sync.RWMutex {
+	m.mutexLock.RLock()
+
+	cl, ok := m.collectionLocks[collection]
+	m.mutexLock.RUnlock()
+
+	if !ok {
+		cl = &sync.RWMutex{}
+		m.mutexLock.Lock()
+		m.collectionLocks[collection] = cl
+		m.mutexLock.Unlock()
+	}
+
+	return cl
+}
---
+++ memj_test.go
@@ -0,0 +1,278 @@
+package memj
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+	var jsonTestPayload = []byte(`{"Name": "Platypus", "Order": "Monotremata"}`)
+
+	var payload map[string]interface{}
+	err := json.Unmarshal(jsonTestPayload, &payload)
+	if err != nil {
+		t.Error("Error unmarshalling: ", err)
+		return
+	}
+
+	memj, _ := New()
+	objectID, err := memj.Insert("TestCollection", payload)
+
+	if err != nil {
+		t.Error("Error inserting document: ", err)
+		return
+	}
+
+	if objectID == "" {
+		t.Error("Invalid objectID")
+		return
+	}
+}
+
+func TestFind(t *testing.T) {
+	var jsonTestPayload = []byte(`{"Name": "Platypus", "Order": "Monotremata"}`)
+
+	var payload map[string]interface{}
+	err := json.Unmarshal(jsonTestPayload, &payload)
+
+	if err != nil {
+		t.Error("Error unmarshalling: ", err)
+		return
+	}
+
+	memj, _ := New()
+	objectID, err := memj.Insert("TestCollection", payload)
+
+	if err != nil {
+		t.Error("Error inserting document: ", err)
+		return
+	}
+
+	if objectID == "" {
+		t.Error("Invalid objectID")
+		return
+	}
+
+	document, err := memj.Find("TestCollection", objectID)
+
+	if err != nil {
+		t.Error("Error in Find: ", err)
+		return
+	}
+
+	returnedObjectID, ok := document["objectid"].(string)
+	if !ok {
+		t.Error("objectID is invalid type")
+		return
+	}
+
+	if returnedObjectID != objectID {
+		t.Error("Wrong object returned!")
+		return
+	}
+}
+
+func TestFindAll(t *testing.T) {
+	var jsonTestPayload = []byte(`{"Name": "Platypus", "Order": "Monotremata"}`)
+
+	var payload map[string]interface{}
+	err := json.Unmarshal(jsonTestPayload, &payload)
+
+	if err != nil {
+		t.Error("Error unmarshalling: ", err)
+		return
+	}
+
+	memj, _ := New()
+	for i := 0; i < 10; i++ {
+		var objectID string
+		objectID, err = memj.Insert("TestCollectionAll", payload)
+
+		if err != nil {
+			t.Error("Error inserting document: ", err)
+			return
+		}
+
+		if objectID == "" {
+			t.Error("Invalid objectID")
+			return
+		}
+	}
+
+	documents, err := memj.FindAll("TestCollectionAll")
+
+	if err != nil {
+		t.Error("Error in Find: ", err)
+		return
+	}
+
+	if len(documents) != 10 {
+		t.Error("Incorrect number of documents returned")
+		return
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	var jsonTestPayload = []byte(`{"Name": "Platypus", "Order": "Monotremata"}`)
+
+	var payload map[string]interface{}
+	err := json.Unmarshal(jsonTestPayload, &payload)
+
+	if err != nil {
+		t.Error("Error unmarshalling: ", err)
+		return
+	}
+
+	memj, _ := New()
+	objectID, err := memj.Insert("TestCollection", payload)
+
+	if err != nil {
+		t.Error("Error inserting document: ", err)
+		return
+	}
+
+	if objectID == "" {
+		t.Error("Invalid objectID")
+		return
+	}
+
+	var updatedTestPayload = []byte(`{"Name": "Fish", "Order": "Monotremata"}`)
+	var updatedPayload map[string]interface{}
+	err = json.Unmarshal(updatedTestPayload, &updatedPayload)
+
+	if err != nil {
+		t.Error("Error unmarshalling updated payload: ", err)
+		return
+	}
+
+	isUpdated, err := memj.Update("TestCollection", objectID, updatedPayload)
+
+	if err != nil {
+		t.Error("Error updating: ", err)
+		return
+	}
+
+	if !isUpdated {
+		t.Error("Failed to update the document")
+		return
+	}
+
+	document, err := memj.Find("TestCollection", objectID)
+
+	if err != nil {
+		t.Error("Error in Find: ", err)
+		return
+	}
+
+	returnedObjectID, ok := document["objectid"].(string)
+	if !ok {
+		t.Error("objectID is invalid type")
+		return
+	}
+
+	if returnedObjectID != objectID {
+		t.Error("Wrong object returned!")
+		return
+	}
+
+	updatedName, ok := document["Name"].(string)
+	if !ok {
+		t.Error("Incorrect type of the updated field")
+		return
+	}
+
+	if updatedName != "Fish" {
+		t.Error("Incorrect updated field")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	var jsonTestPayload = []byte(`{"Name": "Platypus", "Order": "Monotremata"}`)
+
+	var payload map[string]interface{}
+	err := json.Unmarshal(jsonTestPayload, &payload)
+
+	if err != nil {
+		t.Error("Error unmarshalling: ", err)
+		return
+	}
+
+	memj, _ := New()
+	objectID, err := memj.Insert("TestCollection", payload)
+
+	if err != nil {
+		t.Error("Error inserting document: ", err)
+		return
+	}
+
+	if objectID == "" {
+		t.Error("Invalid objectID")
+		return
+	}
+
+	isDeleted, err := memj.Delete("TestCollection", objectID)
+
+	if err != nil {
+		t.Error("Error deleting: ", err)
+		return
+	}
+
+	if !isDeleted {
+		t.Error("Failed to delete the document")
+		return
+	}
+
+	_, err = memj.Find("TestCollection", objectID)
+
+	if err == nil {
+		t.Error("Error: object not deleted")
+		return
+	}
+}
+
+func TestQuery(t *testing.T) {
+	var jsonTestPayload = []byte(`{"Name": "FindMeOut", "Order": "Monotremata"}`)
+
+	var payload map[string]interface{}
+	err := json.Unmarshal(jsonTestPayload, &payload)
+
+	if err != nil {
+		t.Error("Error unmarshalling: ", err)
+		return
+	}
+
+	memj, _ := New()
+	var objectID string
+	objectID, err = memj.Insert("TestCollection", payload)
+
+	if err != nil {
+		t.Error("Error inserting document: ", err)
+		return
+	}
+
+	if objectID == "" {
+		t.Error("Invalid objectID")
+		return
+	}
+
+	var jsonQuery = []byte(`{"Name": "FindMeOut"}`)
+	var queryPayload map[string]interface{}
+	err = json.Unmarshal(jsonQuery, &queryPayload)
+
+	if err != nil {
+		t.Error("Error unmarshalling: ", err)
+		return
+	}
+
+	documents, err := memj.Query("TestCollection", queryPayload)
+
+	if err != nil {
+		t.Error("Error in Find: ", err)
+		return
+	}
+
+	if len(documents) != 1 {
+		t.Error("Incorrect number of documents returned")
+		return
+	}
+}
